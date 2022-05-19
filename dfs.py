5db7164fac3551e911e6163559e38d573a9dfeda
KimjaehoLy <kjaeho8389@naver.com>
2022-05-19 23:29:07 +0900

    공부 내용 추가

---
+++ dfs.py
@@ -0,0 +1,41 @@
+
+
+graph = {
+    1: [2, 3, 4],
+    2: [5],
+    3: [5],
+    4: [],
+    5: [6, 7],
+    6: [],
+    7: [3],
+}
+
+
+def dfs_recursive(node, visited):
+    # 방문처리
+    visited.append(node)
+
+    # 인접 노드 방문
+    for adj in graph[node]:
+        if adj not in visited:
+            dfs_recursive(adj, visited)
+
+    return visited
+
+
+def dfs_stack(start):
+    visited = []
+    # 방문할 순서를 담아두는 용도
+    stack = [start]
+
+    # 방문할 노드가 남아있는 한 아래 로직을 반복한다.
+    while stack:
+        # 제일 최근에 삽입된 노드를 꺼내고 방문처리한다.
+        top = stack.pop()
+        visited.append(top)
+        # 인접 노드를 방문한다.
+        for adj in graph[top]:
+            if adj not in visited:
+                stack.append(adj)
+
+    return visited
---
+++ permutation.py
@@ -0,0 +1,26 @@
+
+
+
+def permute(self, nums: List[int]) -> List[List[int]]:
+    results = []
+    prev_elements = []
+
+    def dfs(elements):
+        if len(elements) == 0:
+            results.append(prev_elements[:])
+
+        for e in elements:
+            next_elements = elements[:]
+            next_elements.remove(e)
+
+            prev_elements.append(e)
+            dfs(next_elements)
+            prev_elements.pop()
+
+    dfs(nums)
+    return results
+
+
+a = [1, 2, 3]
+print(type(a))
+print(permute(a))
